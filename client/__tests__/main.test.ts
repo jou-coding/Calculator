4479c5911ff73386e916f2ec55da1b9dbd68417f
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2025-06-12 12:15:39 +0900

    feat:減算関数と減算のテストの追加

--- client/__tests__/main.test.ts
+++ client/__tests__/main.test.ts
@@ -1,7 +1,11 @@
-import { add } from "../src/main";
+import { add, sub } from "../src/main";
 
 describe("計算のテスト", () => {
   test("足し算のテスト", () => {
     expect(add(2, 3)).toBe(5);
+  });
+
+  test("引き算のテスト", () => {
+    expect(sub(2, 1)).toBe(1);
   });
 });
