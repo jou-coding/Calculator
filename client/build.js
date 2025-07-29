e4c93b09b4ff2f85e68c15aaf055b2fb1db532ea
jou-coding <jou.moriyama05@gmail.com>
2025-07-29 10:13:54 +0900

    chore(build.js): GitHubPages用の公開設定を追加

---
+++ client/build.js
@@ -0,0 +1,44 @@
+import * as esbuild from "esbuild";
+import * as fs from "fs-extra";
+import * as fs02 from "fs";
+
+async function build() {
+  try {
+    // TSファイルをバンドル・コンパイルして dist/main.js に出力
+    await esbuild.build({
+      entryPoints: ["src/main.ts"],
+      bundle: true,
+      outfile: "dist/main.js",
+      sourcemap: true,
+      minify: false,
+    });
+
+    // index.html を dist にコピー
+    await fs.copy("index.html", "dist/index.html");
+    await fs.copy("src/styles/main.css", "dist/main.css");
+    await fs.copy("images/icon.png", "dist/icon.png");
+
+    const filePath = "dist/index.html";
+    let html = fs02.readFileSync(filePath, "utf-8");
+    html = html.replace(/src="dist\/main\.js"/g, 'src="./main.js"');
+    fs02.writeFileSync(filePath, html, "utf-8");
+
+    let css = fs02.readFileSync(filePath, "utf-8");
+    css = css.replace(
+      /href="\.\/src\/styles\/main\.css"/g,
+      'href="./main.css"'
+    );
+    fs02.writeFileSync(filePath, css, "utf-8");
+
+    let icon = fs02.readFileSync(filePath, "utf-8");
+    icon = icon.replace(/href="\.\/images\/icon\.png"/g, 'href="./icon.png"');
+    fs02.writeFileSync(filePath, icon, "utf-8");
+
+    console.log("Build successful!");
+  } catch (err) {
+    console.error("Build failed:", err);
+    process.exit(1);
+  }
+}
+
+build();
